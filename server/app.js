02e6ab54aa40dc56a3095a738d133fb002e16b4c
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2025-08-27 21:03:32 +0200

    preparing backend for prod

--- server/app.js
+++ server/app.js
@@ -30,7 +30,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.static(path.join(__dirname, "public")));
 
 // routes
