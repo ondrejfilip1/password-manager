153ed9f1b0b0d83aa70f8a63c5c08a41728852d6
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2025-08-27 21:14:20 +0200

    Fixed URL typo

--- client/src/utils/GetURL.js
+++ client/src/utils/GetURL.js
@@ -1,5 +1,5 @@
 export const getURL = () => {
   return !process.env.NODE_ENV || process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://password-manager-f5cm.onrender.com/";
+    : "https://password-manager-f5cm.onrender.com";
 };
