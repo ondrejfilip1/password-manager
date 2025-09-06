07655549084693378c358b6287664e307d6a187c
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2025-09-06 16:39:37 +0200

    Better email structure, dependabot

--- server/utils/validators.js
+++ server/utils/validators.js
@@ -1,10 +1,15 @@
-export const validateEmail = (email) => {
+const validateEmail = (email) => {
   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return re.test(email);
 };
 
-export const validateUrl = (url) => {
+const validateUrl = (url) => {
   var re =
     /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
   return re.test(url);
+};
+
+module.exports = {
+  validateEmail,
+  validateUrl,
 };
